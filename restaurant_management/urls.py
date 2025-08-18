5a2ba57d373fa83e4806cde0f89f69564a98196c
Siddhant Saxena <109909335+Sidismyname@users.noreply.github.com>
2025-08-18 19:21:16 +0530

    32800

--- restaurant_management/urls.py
+++ restaurant_management/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import handler404
+from django.shortcuts import render, status
+
+def custom_404_view(request, exception):
+    return render(request,'home/templates/home/404.html', status = 404)
 
 urlpatterns = [
     path('admin/', admin.site.urls),

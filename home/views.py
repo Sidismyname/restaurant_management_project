82f5c9fe18ae3cd761f2fee4d9cd1e36e7e6fd06
Siddhant Saxena <109909335+Sidismyname@users.noreply.github.com>
2025-08-28 18:15:33 +0530

    37779

--- home/views.py
+++ home/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 def about(request):
     return render(request, "home/about.html")
+
+def contact_us_view(request):
+    contact_info = {
+        'restaurant_name':'family-e-dine',
+        'address':'123 street, abc',
+        'phone':'+91-9999999990',
+        'email':'abc@gmail.com',
+        'hours':'9am-11pm :Sunday off'
+    }
+    return render(request,'home/contact.html',{'contact_info':contact_info})
